8e984796179f306488e1f7e23cb186b51973429a
etuhu <andgiz@mail.ru>
2019-11-20 11:40:49 +0700

    Вносит правки в соответствии с баглистом

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var body = document.querySelector('body');
+var footer = document.querySelector('.page-footer');
+var pageFooterEnd = document.querySelector('.page-footer__end');
+
 var modal = document.querySelector('.modal');
 var modalOverlay = document.querySelector('.modal__overlay');
 var modalActivator = document.querySelector('.contacts-menu__button');
@@ -8,10 +12,43 @@ var form = modal.querySelector('.modal__form');
 var username = form.querySelector('#modal-username');
 var phone = form.querySelector('#modal-phonenumber');
 var letter = form.querySelector('#modal-question');
+
 var isStorageSupport = true;
 var storageUsername = '';
 var storagePhone = '';
 var storageLetter = '';
+
+var navigationList = document.querySelector('.navigation__list');
+var navigationToggle = document.querySelector('.navigation__toggle');
+var officeContacts = document.querySelector('.office-contacts__text');
+var officeContactsToggle = document.querySelector('.office-contacts__toggle');
+
+var feedbackPhonenumber = document.getElementById('feedback-phonenumber');
+var modalPhonenumber = document.getElementById('modal-phonenumber');
+
+var phoneMask = 0;
+var promoBlockNote = document.querySelector('.promo-block__note');
+var pageContent = document.querySelector('.page-content');
+var feedbackBlock = document.querySelector('.feedback');
+var promoBlockButton = document.querySelector('.promo-block__button');
+
+promoBlockNote.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  pageContent.scrollIntoView(top);
+});
+
+promoBlockButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  feedbackBlock.scrollIntoView(top);
+});
+
+phoneMask = IMask(feedbackPhonenumber, {
+  mask: '+{7}(000)000-00-00'
+});
+
+phoneMask = IMask(modalPhonenumber, {
+  mask: '+{7}(000)000-00-00'
+});
 
 try {
   storageUsername = localStorage.getItem('username');
@@ -24,6 +61,7 @@ try {
 modalActivator.addEventListener('click', function (evt) {
   evt.preventDefault();
   modal.classList.add('modal--active');
+  body.classList.add('scroll-hidden');
   username.focus();
   if (storageUsername) {
     username.value = storageUsername;
@@ -39,12 +77,14 @@ modalActivator.addEventListener('click', function (evt) {
 closeModal.addEventListener('click', function (evt) {
   evt.preventDefault();
   modal.classList.remove('modal--active');
+  body.classList.remove('scroll-hidden');
 });
 
 modalOverlay.addEventListener('click', function (evt) {
   evt.preventDefault();
   if (modal.classList.contains('modal--active')) {
     modal.classList.remove('modal--active');
+    body.classList.remove('scroll-hidden');
   }
 });
 
@@ -53,6 +93,7 @@ window.addEventListener('keydown', function (evt) {
     evt.preventDefault();
     if (modal.classList.contains('modal--active')) {
       modal.classList.remove('modal--active');
+      body.classList.remove('scroll-hidden');
     }
   }
 });
@@ -62,5 +103,37 @@ form.addEventListener('submit', function () {
     localStorage.setItem('username', username.value);
     localStorage.setItem('phone', phone.value);
     localStorage.setItem('letter', letter.value);
+  }
+});
+
+navigationToggle.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  navigationList.classList.toggle('navigation__list--unvisible');
+  navigationToggle.classList.toggle('display-toggle--minimized');
+  footer.classList.toggle('page-footer--shift');
+  navigationToggle.blur();
+  if (!officeContacts.classList.contains('office-contacts__text--unvisible')) {
+    officeContacts.classList.add('office-contacts__text--unvisible');
+    pageFooterEnd.classList.add('page-footer__end--shrink');
+  }
+  if (!officeContactsToggle.classList.contains('display-toggle--minimized')) {
+    officeContactsToggle.classList.add('display-toggle--minimized');
+  }
+});
+
+officeContactsToggle.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  officeContacts.classList.toggle('office-contacts__text--unvisible');
+  officeContactsToggle.classList.toggle('display-toggle--minimized');
+  pageFooterEnd.classList.toggle('page-footer__end--shrink');
+  officeContactsToggle.blur();
+  if (footer.classList.contains('page-footer--shift')) {
+    footer.classList.remove('page-footer--shift');
+  }
+  if (!navigationList.classList.contains('navigation__list--unvisible')) {
+    navigationList.classList.add('navigation__list--unvisible');
+  }
+  if (!navigationToggle.classList.contains('display-toggle--minimized')) {
+    navigationToggle.classList.add('display-toggle--minimized');
   }
 });
